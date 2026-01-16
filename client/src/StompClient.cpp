e95962bb7a2400244648cfce65edc03509cf5bb9
sarit <sarityar@post.bgu.ac.il>
2026-01-16 22:34:56 +0000

    changes

--- client/src/StompClient.cpp
+++ client/src/StompClient.cpp
@@ -7,59 +7,53 @@ using namespace std;
 
 
 int main(int argc, char *argv[]) {
-	if (argc < 3) {
-        std::cerr << "Usage: " << argv[0] << " host port" << std::endl << std::endl;
+    if (argc < 3) {
+        std::cerr << "Usage: " << argv[0] << " host port" << std::endl;
         return -1;
     }
     std::string host = argv[1];
     short port = atoi(argv[2]);
 
-	StompProtocol protocol;
+    StompProtocol protocol;
     ConnectionHandler connectionHandler(host, port);
 
     if (!connectionHandler.connect()) {
         std::cerr << "Cannot connect to " << host << ":" << port << std::endl;
         return 1;
     }
-	
-	
-	// user input and sending thread	
-	std::thread inputThread([&connectionHandler, &protocol](){
-		
-		while (!protocol.shouldTerminate()) {
-			
-		
-			std::string line;
-			std::getline(cin, line);
+    
+    // Thread לקריאת קלט מהמקלדת ושליחה לשרת
+    std::thread inputThread([&connectionHandler, &protocol](){
+        while (!protocol.shouldTerminate()) {
+            std::string line;
+            if (!std::getline(cin, line)) break;
 
-			std::string frameToSend = protocol.processKeyboardInput(line);
-			if(!frameToSend.empty()){
-				if (!connectionHandler.sendFrameAscii(frameToSend, '\0')) {
-					std::cout << "Disconnected. Exiting...\n" << std::endl;
-					break;
-				}
-			}
-		}
-		
-	});	
-		
-	// receiving thread
-	std::thread receiveThread([&connectionHandler, &protocol](){
+            std::string frameToSend = protocol.processKeyboardInput(line);
+            if (!frameToSend.empty()) {
+                if (!connectionHandler.sendBytes(frameToSend.c_str(), frameToSend.length())) {
+                    std::cout << "Disconnected from server." << std::endl;
+                    break;
+                }
+            }
+        }
+    }); 
+        
+    std::thread receiveThread([&connectionHandler, &protocol](){
+        while (!protocol.shouldTerminate()) {
+            std::string answer;
 
-		while (!protocol.shouldTerminate()) {
-			
-			std::string answer;
+            if (!connectionHandler.getFrameAscii(answer, '\0')) {
+                if (!protocol.shouldTerminate()) {
+                    std::cout << "Server disconnected unexpectedly." << std::endl;
+                }
+                break;
+            }
+            protocol.processServerResponse(answer);
+        }
+    });
 
-			if (!connectionHandler.getFrameAscii(answer, '\0')) {
-				std::cout << "Disconnected. Exiting...\n" << std::endl;
-				break;
-			}
-			protocol.processServerResponse(answer);
-		}
-	});
+    inputThread.join();
+    receiveThread.join();
 
-	inputThread.join();
-	receiveThread.join();
-
-	return 0;
+    return 0;
 }
